ade4aca937a6bad1fb7e4ccc75b017a3eba96464
Braden Groom <braden.groom@gmail.com>
2014-10-03 18:33:59 -0500

    adding an error check for when I run out of requests

--- downloader.js
+++ downloader.js
@@ -17,36 +17,51 @@ function  getRepos(since) {
   //get repositories since last id
   client.get('/repositories', {"since": since}, function (err, status, body, headers) {
 
-    //loop over each repository
-    for (var i = 0; i < body.length; i++) {
+    if (typeof body === 'undefined') {
+      console.log("Out of requests.");
+      console.log("More requests available at: ");
+      client.get('/rate_limit', {}, function (err, status, body, headers) {
 
-      var currRepo = body[i];
+        //convert time since epoch to date
+        var d = new Date(0);
+        d.setUTCSeconds(body.rate.reset);
 
-      //make sure that the repo is public, from a user (not organization), and not a fork
-      if (!currRepo.private && currRepo.owner.type == 'User' && !currRepo.fork) {
+        //print reset time
+        console.log(d.toUTCString());
+      });
+    } else {
 
-        //get the repo information
-        client.get('/repos/' + currRepo.owner.login + '/' + currRepo.name, {}, function (err, status, body, headers) {
+      //loop over each repository
+      for (var i = 0; i < body.length; i++) {
 
-          //if there was no error and the repo is written in C++ or C
-          if(!err && (body.language == 'C++' || body.language == 'C' )){
+        var currRepo = body[i];
 
-            /*client.get('/repos/' +body.owner.login + '/' + body.name + '/contributors', {}, function (err, status, body, headers) {
+        //make sure that the repo is public, from a user (not organization), and not a fork
+        if (!currRepo.private && currRepo.owner.type == 'User' && !currRepo.fork) {
 
-              //if there was no error, there is only one contributor, and there were at least 10 contributions
-              if (!err && body.length == 1 && body[0].contributions >= 10) {
-                console.log(body[]);
-                console.log(body[0].id + " : " + currRepo.html_url);
-              }
-            });*/
+          //get the repo information
+          client.get('/repos/' + currRepo.owner.login + '/' + currRepo.name, {}, function (err, status, body, headers) {
 
-            console.log(body.id,body.html_url);
-          }
-        });
+            //if there was no error and the repo is written in C++ or C
+            if(!err && (body.language == 'C++' || body.language == 'C' )){
+
+              /*client.get('/repos/' +body.owner.login + '/' + body.name + '/contributors', {}, function (err, status, body, headers) {
+
+                //if there was no error, there is only one contributor, and there were at least 10 contributions
+                if (!err && body.length == 1 && body[0].contributions >= 10) {
+                  console.log(body[]);
+                  console.log(body[0].id + " : " + currRepo.html_url);
+                }
+              });*/
+
+              console.log(body.id,body.html_url);
+            }
+          });
+        }
       }
-    }
 
-    getRepos(body[body.length-1].id);
+      getRepos(body[body.length-1].id);
+    }
   });
 }
 
