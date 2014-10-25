0a2565d1a631fd59b05303e79f748871fa691e01
Braden Groom <braden.groom@gmail.com>
2014-10-25 18:16:43 -0500

    removing some jslint issues

--- downloader.js
+++ downloader.js
@@ -1,96 +1,102 @@
-var github = require('octonode');
-var fs = require('fs');
+/*global require, console*/
 
-//read in config.json /*379396*/
-var config = require('./config.json');
+(function () {
+    "use strict";
 
-//create github client
-var client = github.client({
-    id: config.clientID,
-    secret: config.clientSecret
-});
+    var github = require('octonode'),
+        fs = require('fs'),
 
-var db = require('orchestrate')(config.orchestrateKey);
+        //read in config.json /*379396*/
+        config = require('./config.json'),
 
-getRepos(config.since);
+        //create github client
+        client = github.client({
+            id: config.clientID,
+            secret: config.clientSecret
+        }),
 
-function getRepos(since) {
+        db = require('orchestrate')(config.orchestrateKey);
 
-    //get repositories since last id
-    client.get('/repositories', {
-        "since": since
-    }, function (err, status, body, headers) {
+    function saveState(since) {
 
-        if (typeof body === 'undefined') {
-            console.log("Out of requests.");
-            console.log("More requests available at: ");
-            client.get('/rate_limit', {}, function (err, status, body, headers) {
+        config.since = since;
 
-                //convert time since epoch to date
-                var d = new Date(0);
-                d.setUTCSeconds(body.resources.core.reset);
-
-                //print reset time
-                console.log(d.toUTCString());
-
-                //if we have seen new repos
-                if (config.since !== since) {
-
-                    //save our state
-                    saveState(since)
-                }
-            });
-        } else {
-
-            //loop over each repository
-            for (var i = 0; i < body.length; i++) {
-
-                var currRepo = body[i];
-
-                //make sure that the repo is public, from a user (not organization), and not a fork
-                if (!currRepo.private && currRepo.owner.type == 'User' && !currRepo.fork) {
-
-                    //get the repo information
-                    client.get('/repos/' + currRepo.owner.login + '/' + currRepo.name, {}, function (err, status, body, headers) {
-
-                        //if there was no error and the repo is written in C++ or C
-                        if (!err && (body.language == 'C++' || body.language == 'C')) {
-
-                            (function (repo) {
-                                client.get('/repos/' + body.owner.login + '/' + body.name + '/contributors', {}, function (err, status, body, headers) {
-
-                                    //if there was no error, there is only one contributor, and there were at least 10 contributions
-                                    if (!err && body.length == 1 && body[0].contributions >= 10) {
-                                        console.log(repo.id + " : " + repo.html_url);
-                                        db.put(config.collection, repo.id, repo)
-                                            .then(function (result) {
-
-                                            })
-                                            .fail(function (err) {
-                                                console.log(err);
-                                            })
-                                    }
-                                });
-                            })(body);
-                        }
-                    });
-                }
+        fs.writeFile('config.json', JSON.stringify(config, null, 4), function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("State saved in config.json");
             }
+        });
+    }
 
-            getRepos(body[body.length - 1].id);
-        }
-    });
-}
+    function getRepos(since) {
 
-function saveState(since) {
+        //get repositories since last id
+        client.get('/repositories', {
+            "since": since
+        }, function (err, status, body, headers) {
 
-    config.since = since;
+            if (typeof body === 'undefined') {
+                console.log("Out of requests.");
+                console.log("More requests available at: ");
+                client.get('/rate_limit', {}, function (err, status, body, headers) {
 
-    fs.writeFile('config.json', JSON.stringify(config, null, 4), function (err) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log("State saved in config.json");
-        }
-    });
-}
+                    //convert time since epoch to date
+                    var d = new Date(0);
+                    d.setUTCSeconds(body.resources.core.reset);
+
+                    //print reset time
+                    console.log(d.toUTCString());
+
+                    //if we have seen new repos
+                    if (config.since !== since) {
+
+                        //save our state
+                        saveState(since);
+                    }
+                });
+            } else {
+
+                //loop over each repository
+                for (var i = 0; i < body.length; i++) {
+
+                    var currRepo = body[i];
+
+                    //make sure that the repo is public, from a user (not organization), and not a fork
+                    if (!currRepo.private && currRepo.owner.type == 'User' && !currRepo.fork) {
+
+                        //get the repo information
+                        client.get('/repos/' + currRepo.owner.login + '/' + currRepo.name, {}, function (err, status, body, headers) {
+
+                            //if there was no error and the repo is written in C++ or C
+                            if (!err && (body.language == 'C++' || body.language == 'C')) {
+
+                                (function (repo) {
+                                    client.get('/repos/' + body.owner.login + '/' + body.name + '/contributors', {}, function (err, status, body, headers) {
+
+                                        //if there was no error, there is only one contributor, and there were at least 10 contributions
+                                        if (!err && body.length == 1 && body[0].contributions >= 10) {
+                                            console.log(repo.id + " : " + repo.html_url);
+                                            db.put(config.collection, repo.id, repo)
+                                                .then(function (result) {
+
+                                                })
+                                                .fail(function (err) {
+                                                    console.log(err);
+                                                })
+                                        }
+                                    });
+                                })(body);
+                            }
+                        });
+                    }
+                }
+
+                getRepos(body[body.length - 1].id);
+            }
+        });
+    }
+
+    getRepos(config.since);
+})();
